4cdb10b4592840014f01f9586f8bc77111692496
Lalit <lalit.garghate@gmail.com>
2021-05-28 07:09:06 +0530

    beforeEach

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,16 @@ import App from "./App";
 import CommentBox from "./components/CommentBox/CommentBox";
 import CommentList from "./components/CommentList/CommentList";
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
 test("should contain instance of CommentBox", () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentBox).length).toBe(1);
 });
 
 test("should contain instance of CommentList", () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentList).length).toBe(1);
 });
